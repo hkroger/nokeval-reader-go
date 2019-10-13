bc7ba8a2ff33b2e6721107bb348267afbd057f57
Hannu Kroger <hkroger@gmail.com>
2019-10-13 16:57:15 +0300

    Deb build stuff

---
+++ build_deb.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+# Setup
+cd "$(dirname $0)"
+ROOT=$(pwd)
+rm -rf debroot|| echo "no debroot to delete. good."
+TARGET=$ROOT/debroot/nokeval_reader/
+READER_TARGET=$TARGET/opt/nokeval_reader/
+SERVICE_TARGET=$TARGET/etc/systemd/system/
+mkdir -p "$TARGET"
+mkdir -p "$READER_TARGET"
+mkdir -p "$SERVICE_TARGET"
+
+# Start building
+GOOS=linux GOARCH=arm GOARM=5 go build -o nokeval-reader-arm cmd/reader/main.go
+
+# Package
+cp nokeval_reader_arm "$READER_TARGET"/nokeval_reader
+cp -r DEBIAN "$TARGET"
+cp -r LICE* "$READER_TARGET"
+cp -r READ* "$READER_TARGET"
+cp -r config.yaml.example "$READER_TARGET"
+cp nokeval_reader.service "$SERVICE_TARGET"
+
+cd debroot
+
+dpkg -b nokeval_reader
+
+VERSION=$(dpkg -I nokeval_reader.deb |grep Version|sed -e 's/Version: //g' -e 's/ //g')
+
+mv nokeval_reader.deb nokeval_reader_$VERSION.deb
---
+++ upload_deb.sh
@@ -0,0 +1,4 @@
+# shellcheck disable=SC2164
+cd "$(dirname $0)"
+scp debroot/*.deb lakka.kapsi.fi:public_html/debs/stretch
+ssh lakka.kapsi.fi bin/update_debs.sh
